55d07448290c66834c3bd13e3c7ecd294042bc3a
Pete Davis <petedavis327@gmail.com>
2024-11-12 09:01:53 -0500

    add pre commit hooks

--- util/embedding.py
+++ util/embedding.py
@@ -3,26 +3,33 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 
+
 def init_embedding_model():
-   embedding = HuggingFaceEmbeddings(model_name=os.getenv('EMBEDDING_MODEL')) 
-   return embedding
+    embedding = HuggingFaceEmbeddings(model_name=os.getenv("EMBEDDING_MODEL"))
+    return embedding
+
 
 # load pdfs from directory as langchain documents
 def load_pdf_docs(pdf_directory_path):
-  """
-  Load PDF documents from the specified directory
-  Returns:
-  Langchain Document objects.
-  """
-  # Initialize PDF loader with specified directory
-  loader = PyPDFDirectoryLoader(pdf_directory_path) 
-  # Load PDF files as langchain docs
-  loaded_docs = loader.load()
-  return loaded_docs
+    """
+    Load PDF documents from the specified directory
+    Returns:
+    Langchain Document objects.
+    """
+    # Initialize PDF loader with specified directory
+    loader = PyPDFDirectoryLoader(pdf_directory_path)
+    # Load PDF files as langchain docs
+    loaded_docs = loader.load()
+    return loaded_docs
+
 
 # recursively split and chunk langchain documents
 def rec_split_chunk(documents, chunk_size, chunk_overlap):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap, length_function=len, add_start_index=True)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+        add_start_index=True,
+    )
     chunked_docs = text_splitter.split_documents(documents)
     return chunked_docs
-
