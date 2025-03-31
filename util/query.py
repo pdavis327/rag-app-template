56c908cd2a3a30e93dda7f29fbf4cb878845d14b
pdavis327 <petedavis327@gmail.com>
2025-03-31 17:02:44 -0400

    cleanup

--- util/query.py
+++ util/query.py
@@ -9,18 +9,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-prompt_template = ChatPromptTemplate.from_template(
-    """
-  Answer the question based ONLY on the following context:
-  {context}
-  - -
-  Given the context information with no prior knowledge,
-  answer the question. If you can't answer the question
-  with the context information, don't try.
-  Original question: {question}
-  """
-)
-
 chat_history_template = ChatPromptTemplate.from_messages(
     [
         (
@@ -47,44 +35,6 @@ def format_docs(docs):
 def init_llm():
     llm = OllamaLLM(model=os.getenv("LLM"))
     return llm
-
-
-def query_rag(Chroma_collection, query_text, llm_model, promp_template):
-    """
-    Query a Retrieval-Augmented Generation (RAG) system using Chroma db.
-    Args:
-      - query_text (str): The text to query the RAG system with.
-      -prompt_template (str): Query prompt template
-      inclding context and question
-    Returns:
-      - formatted_response (str): Formatted response
-      including the generated text and sources.
-      - response_text (str): The generated response text.
-    """
-
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
-
-    db = Chroma_collection
-    prompt = promp_template
-
-    retriever = db.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={"score_threshold": 0.5, "k": 5},
-    )
-
-    rag_chain = (
-        {"context": retriever | format_docs, "question": RunnablePassthrough()}
-        | prompt
-        | llm_model
-        | StrOutputParser()
-    )
-
-    for chunk in rag_chain.stream(query_text):
-        print(chunk, end="", flush=True)
-
-    return
-
 
 def query_rag_streamlit(Chroma_collection, llm_model, promp_template):
     """
